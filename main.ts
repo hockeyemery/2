a8928d326731bbc6d6b7fc3af142f359e1cef6aa
hockeyemery <113910751+hockeyemery@users.noreply.github.com>
2022-09-19 08:59:08 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,20 +3,27 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(fav)
 })
 input.onGesture(Gesture.TiltLeft, function () {
-    pixel.change(LedSpriteProperty.X, -1)
+    if (_22 == 1) {
+        pixel.change(LedSpriteProperty.X, -1)
+    }
 })
 input.onButtonPressed(Button.AB, function () {
     pixel = game.createSprite(2, 2)
+    _22 = 1
 })
 input.onButtonPressed(Button.B, function () {
     fav += 1
     basic.showNumber(fav)
 })
 input.onGesture(Gesture.TiltRight, function () {
-    pixel.change(LedSpriteProperty.X, 1)
+    if (_22 == 1) {
+        pixel.change(LedSpriteProperty.X, 1)
+    }
 })
 let pixel: game.LedSprite = null
 let fav = 0
+let _22 = 0
 basic.showIcon(IconNames.Heart)
 let word = "hello"
 fav = 8
+_22 = 0
